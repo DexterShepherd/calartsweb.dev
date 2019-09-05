825cdb594be36afe5bdf50d1abc5f640619f932e
dexter <dexterjshepherd@gmail.com>
2019-09-04 18:51:35 -0700

    adding dummy index

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import Welcome from './Routes/Home'
 import Week1 from './Routes/Week1'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -6,10 +7,11 @@ const App = () => {
   return (
     <div className="App">
       <Router>
-        <Route path='/week1/:index?' component={Week1}/>
+        <Route path="/week1/:index?" component={Week1} />
+        <Route path="/" exact component={Welcome} />
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Routes/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => {
+  return <h1>Media and Web Development</h1>
+}
+
+export default Home
